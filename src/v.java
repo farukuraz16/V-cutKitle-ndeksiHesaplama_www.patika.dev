9ba2c7b0cc6dd2d418624fb483fb75c5c21d04c6
farukuzgun <farukuzgun@gmail.com>
2022-10-30 10:57:30 -0400

    ptk

--- src/v.java
+++ src/v.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class v {
+    public static void main(String[] args) {
+/*
+Vücut Kitle İndeksi Hesaplama
+Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+Formül
+Kilo (kg) / Boy(m) * Boy(m)
+
+Çıktısı
+Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
+Lütfen kilonuzu giriniz : 105
+Vücut Kitle İndeksiniz : 35.49215792320173
+ */
+
+        Scanner dat = new Scanner(System.in);
+        System.out.println("boy (m)=");
+        double boy= dat.nextDouble();
+        System.out.println("kilo (m)=");
+        double kilo= dat.nextDouble();
+        double indeks= kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz= "+indeks);
+    }
 }
